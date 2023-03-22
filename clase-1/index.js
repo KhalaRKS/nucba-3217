386259ea35b64bd4debcc572d06d6240e0b11d1b
Albano Kainer <albanokainer@hotmail.com>
2023-03-22 09:15:41 -0300

    primera clase

---
+++ clase-1/index.js
@@ -0,0 +1,49 @@
+var noTieneNada;
+const CONTENEDOR_SECUNDARIO = document.getElementById("contenedor-secundario");
+
+let nombre = "Ender"; // diferencias mayus y minusculas
+let apellido = "Romero";
+let edad = 21;
+let dinero = 55000.52;
+
+let nombreCompleto = nombre + " " + apellido;
+
+let sinValor;
+//          Ender Romero
+alert("Hola estoy interrumpiendo el programa, no deberias ver las consolas");
+console.log("Ender ahora tiene esta cantidad de dinero: ", dinero + 150);
+console.log("Suma de String con Number", 10 + "hola");
+console.log(sinValor);
+
+let millasViaje = 0.00000000000000000000000000000000000000000001;
+
+let notANumber = NaN;
+let infinito = Infinity;
+
+let nombredeAlumno = "Ender Romero";
+let booleanoTrue = true;
+let booleanoFalse = false;
+
+let string = "Gomez";
+let saludo = "Hola como estas?";
+
+let numero2 = "21";
+let numero3 = 21;
+let numero4 = 21;
+let numero5 = 21;
+
+let valorIngresadoPorTeclado = prompt("Ingrese su nombre");
+
+console.log(valorIngresadoPorTeclado);
+console.log(valorIngresadoPorTeclado);
+console.log(valorIngresadoPorTeclado);
+
+// Hola esto es un comentarioi de una sola linea
+/*
+Hola esto es
+un comentario 
+multi linea
+asd
+
+
+*/
