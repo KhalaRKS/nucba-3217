a50c5fe12761dc629504b7350535830478a76c78
albakainer <albanokainer@hotmail.com>
2023-05-16 20:04:54 -0300

    clase 16 repaso

---
+++ clase-16/index.js
@@ -0,0 +1,66 @@
+// class Pizza {
+//   // Propiedades
+//   id = 1;
+//   sabor;
+//   toppings;
+//   tamanio;
+//   constructor(sabor, toppings, tamanio) {
+//     this.sabor = sabor;
+//     this.toppings = toppings;
+//     this.tamanio = tamanio;
+//   }
+
+//   // Métodos
+//   hablar() {
+//     return "Odio los lunes.";
+//   }
+// }
+// let array = [
+//   {
+//     sabor: "muzzarella",
+//     tamanio: "grande",
+//     toppings: ["asd", "asd", "asd"],
+//   },
+//   {
+//     sabor: "muzzarella",
+//     tamanio: "grande",
+//     toppings: ["asd", "asd", "asd"],
+//   },
+//   {
+//     sabor: "muzzarella",
+//     tamanio: "grande",
+//     toppings: ["asd", "asd", "asd"],
+//   },
+//   {
+//     sabor: "muzzarella",
+//     tamanio: "grande",
+//     toppings: ["asd", "asd", "asd"],
+//   },
+// ];
+
+// let Pizzas = [
+//   new Pizza(
+//     "Muzzarella",
+//     ["aceitunas", "Cebolla", "cuadraditos de panceta"],
+//     "Grande"
+//   ),
+//   new Pizza(
+//     "Muzzarella",
+//     ["aceitunas", "Cebolla", "cuadraditos de panceta"],
+//     "Grande"
+//   ),
+//   new Pizza(
+//     "Muzzarella",
+//     ["aceitunas", "Cebolla", "cuadraditos de panceta"],
+//     "Grande"
+//   ),
+// ];
+
+// let animal = {
+//   name: "Garfield",
+//   type: "Cat",
+// };
+
+// console.log(Pizzas);
+// function asignarNombre(nombre, persona) {}
+// asignarNombre("pepe", "Juansito");
